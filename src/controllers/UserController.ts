ec100fd9ca227998229c4c5d643e59bcf36c5e65
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-02-26 20:45:39 -0300

    [NPS] Validações e template de e-mail - Adicionando lib  de validação yup - Adicionando rotas /answers e /nps - Adicionando controllers AnswerController e NpsController - Adicionando validação no UserController - Atualizando template do e-mail

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,9 +2,27 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 
+import * as yup from 'yup';
+
 class UserController {
   async create(request: Request, response: Response){
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    })
+
+    // if(!(await !schema.isValid(request.body)))
+    //   return response.status(400).json({ error: "Validation failed!" });
+
+    // Outra forma de validar
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
+
 
     const usersRepository = getCustomRepository(UsersRepository);
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,20 @@ import { Router } from 'express';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
+
 const surveysController = new SurveysController();
+
 const sendMailController = new SendMailController();
+
+const answerController = new AnswerController();
+
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -15,5 +23,9 @@ router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
