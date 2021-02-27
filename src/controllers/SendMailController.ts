e6a8cfd58c76ce8d599ce99cb7cfd43fac96f32c
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-02-27 11:16:21 -0300

    [NPS] - Removendo scritp hook e adicionando nova dependência - Adicionando middleware - Adicionado função afterAll nos testes - Refatorando. Adicionando classe customizada de error - Criando classe de erros customizada

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -5,6 +5,7 @@ import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
+import { AppError } from "../errors/AppError";
 
 class SendMailController {
   async execute(request: Request, response: Response){
@@ -16,18 +17,14 @@ class SendMailController {
 
     const user = await usersRepository.findOne({ email });
 
-    if(!user){
-      return response.status(400).json({
-        error: "User does not exists",
-      });
+    if(!user) {
+      throw new AppError("User does not exists");
     }
 
     const survey = await surveysRepository.findOne({id: survey_id});
 
-    if(!survey){
-      return response.status(400).json({
-        error: "Survey does not exists",
-      })
+    if(!survey) {
+      throw new AppError("Survey does not exists");
     }
 
     const npsPath = resolve(__dirname, "..", "views", "email", "npsMail.hbs");
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
   async create(request: Request, response: Response){
@@ -20,7 +21,7 @@ class UserController {
     try {
       await schema.validate(request.body, { abortEarly: false })
     } catch (error) {
-      return response.status(400).json({ error });
+      throw new AppError(error);
     }
 
 
@@ -31,9 +32,7 @@ class UserController {
     })
 
     if (userAlreadyExists) {
-      return response.status(400).json({
-        error: "User already exists!",
-      });
+      throw new AppError("User already exists!");
     }
 
     const user = usersRepository.create({
